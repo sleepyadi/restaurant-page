d579726fcca16668028d5aabfed6c9218e9f91c7
adi <xdragon.0601@outlook.com>
2022-09-08 16:07:30 +0530

    Add webpack and config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+
+module.exports = {
+    mode: 'development',
+    
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js') 
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    }
+}
