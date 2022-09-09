8ae288e0197ee5d7e828b761ed2b347f9d4a2fa2
adi <xdragon.0601@outlook.com>
2022-09-09 17:11:32 +0530

    Add styling to tabs and about component

--- src/TEXT_DATA.js
+++ src/TEXT_DATA.js
@@ -16,5 +16,12 @@ const MENU = [
 
 ]
 
+const ABOUT = {
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ,
 
-export {MENU}
+    contact: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+
+}
+
+
+export {MENU, ABOUT }
--- src/about.js
+++ src/about.js
@@ -1,4 +1,5 @@
 import { LayoutCreator, CardCreator } from "./utils";
+import { ABOUT } from "./TEXT_DATA";
 
 
 function createAboutCard() {
@@ -6,12 +7,12 @@ function createAboutCard() {
     const aboutUs = new CardCreator('about__about');
 
     const aboutTitle = document.createElement('h2');
-    aboutTitle.classList.add('about__about-title');
+    aboutTitle.classList.add('about__title');
     aboutTitle.textContent = 'About Us';
 
     const aboutInfo = document.createElement('p');
     aboutInfo.classList.add('about__about-para');
-    aboutInfo.textContent = '';
+    aboutInfo.textContent = ABOUT.about;
 
     aboutUs.addElement([aboutTitle, aboutInfo]);
 
@@ -23,12 +24,12 @@ function createContactCard() {
     const contact = new CardCreator('about__contact');
 
     const contactTitle = document.createElement('h2');
-    contactTitle.classList.add('about__contact-title');
+    contactTitle.classList.add('about__title');
     contactTitle.textContent = 'Contact Us';
 
     const contactInfo = document.createElement('p');
     contactInfo.classList.add('about__contact-para');
-    contactInfo.textContent = '';
+    contactInfo.textContent = ABOUT.contact;
 
     contact.addElement([contactTitle, contactInfo]);
 
